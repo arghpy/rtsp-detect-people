4fd914ad508ede2501d374d18d1e6eeb156ad119
arghpy <subaandrei2@gmail.com>
2026-03-03 10:04:09 +0200

    debug

--- app/rtsp_detect_people.py
+++ app/rtsp_detect_people.py
@@ -162,6 +162,7 @@ if __name__ == "__main__":
     while True:
         # Run model on frame
         video_frame, PERSON_DETECTED = process_frame(CONFIG["RTSP_URL"])
+        print(f"size: {video_frame.size}, {video_frame.shape[0]} {video_frame.shape[1]}")
 
         if PERSON_DETECTED and not OCCUPANCY_DETECTED:
             OCCUPANCY_DETECTED = True
