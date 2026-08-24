653a963813fd05371b98ae9835549b9afa0d19d1
arghpy <subaandrei2@gmail.com>
2026-08-24 08:17:54 +0300

    fix: try to fix reconnecting to stream

--- app/utils/video.py
+++ app/utils/video.py
@@ -1,6 +1,7 @@
 import queue
 import subprocess
 import time
+import sys
 
 import cv2
 import numpy as np
@@ -10,26 +11,35 @@ import app.yolo.cuda
 
 def collect_frames(cap, rtsp_url, frame_queue, STOP_EVENT):
     failed_frames = 0
+
     while not STOP_EVENT.is_set():
         ret, frame = cap.read()
-        if failed_frames > 20:
-            app.utils.logger.eprint("Restarting cap")
-            while not cap.isOpened():
-                app.utils.logger.eprint(f"Could not read from {rtsp_url}")
-                cap.open(rtsp_url)
-            failed_frames = 0
-
         if not ret:
-            app.utils.logger.eprint("Could not read frame.")
             failed_frames += 1
-            time.sleep(1)
+            app.utils.logger.eprint(
+                f"Could not read frame ({failed_frames}/20)"
+            )
+
+            if failed_frames >= 20:
+                app.utils.logger.pprint("Reconnecting to camera...")
+                cap.release()
+                cap = cv2.VideoCapture(rtsp_url)
+                if cap.isOpened():
+                    app.utils.logger.pprint("Camera reconnected")
+                    failed_frames = 0
+                else:
+                    app.utils.logger.eprint(
+                        "Could not reconnect to camera"
+                    )
+                time.sleep(1)
+
             continue
-        else:
-            try:
-                frame_queue.put(frame, timeout=1)
-            except queue.Full:
-                # drop frame if queue full
-                pass
+
+        failed_frames = 0
+        try:
+            frame_queue.put(frame, timeout=1)
+        except queue.Full:
+            pass
 
 
 def probe_stream(rtsp_url) -> tuple[int, int, int]:
